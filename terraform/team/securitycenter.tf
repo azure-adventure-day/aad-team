3322a93f692876923bee8cd7b8eaab231110c145
denniszielke <11569044+denniszielke@users.noreply.github.com>
2020-10-28 11:32:14 +0100

    removed security center

--- terraform/team/securitycenter.tf
+++ terraform/team/securitycenter.tf
@@ -1,6 +1,6 @@
-resource "null_resource" "EnableSecurityCenter" {
-  provisioner "local-exec" {
-    command = "az security pricing create -n KubernetesService --tier 'standard'"
+# resource "null_resource" "EnableSecurityCenter" {
+#   provisioner "local-exec" {
+#     command = "az security pricing create -n KubernetesService --tier 'standard'"
     
-  }
-}
+#   }
+# }
