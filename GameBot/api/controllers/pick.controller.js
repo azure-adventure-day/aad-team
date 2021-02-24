8abee0d60aa11d67d1074966c0e2c6c3621ffbbd
Ricardo Niepel <ricardo.niepel@outlook.com>
2021-02-24 09:50:19 +0100

    add turn

--- GameBot/api/controllers/pick.controller.js
+++ GameBot/api/controllers/pick.controller.js
@@ -2,35 +2,38 @@ const PickStrategyFactory = require('../services/pickStrategyFactory.service');
 const appInsights = require("applicationinsights");
 
 const pick = async (req, res) => {
-    var Player1Name = req.body.Player1Name;
     var matchId = req.body.MatchId;
-    if (Player1Name == undefined || matchId == undefined) {
+    var player1Name = req.body.Player1Name;
+    var turn = req.body.Turn;
+    if (player1Name == undefined || matchId == undefined) {
         res.status(400);
         res.send("Player1NamerId or MatchId undefined");
         return;
     }
 
-
     const strategyOption = process.env.PICK_STRATEGY || "RANDOM";
     const result = pickFromStrategy(strategyOption);
-    
-    // implement arcade intelligence here
-    // if (Player1Name == "Dud") {
+
+    // TODO: implement arcade intelligence here, see also ./GameBot/README.md for sample requests    
+    // if (player1Name == "Dud" && turn == 0) {
+    //    strategyOption = "CUSTOM";
     //    result.text = "rock";
     //    result.bet = 1;
     // }
-    
-    console.log('Against '+Player1Name+', strategy ' + strategyOption + '  played ' + result.text);
-    
+
+    console.log('Against ' + player1Name + ', strategy ' + strategyOption + '  played ' + result.text);
+
     const applicationInsightsIK = process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
     if (applicationInsightsIK) {
-        if (appInsights && appInsights.defaultClient)
-        {
+        if (appInsights && appInsights.defaultClient) {
             var client = appInsights.defaultClient;
             client.commonProperties = {
                 strategy: strategyOption
             };
-            client.trackEvent({name: "pick", properties: {matchId: matchId, strategy: strategyOption, move: result.text, player: Player1Name, bet: result.bet}});
+            client.trackEvent({
+                name: "pick", properties:
+                    { matchId: matchId, strategy: strategyOption, move: result.text, player: player1Name, bet: result.bet }
+            });
         }
     }
     res.send({ "Move": result.text, "Bet": result.bet });
