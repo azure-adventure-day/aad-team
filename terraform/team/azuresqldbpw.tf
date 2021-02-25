a27438a331fe7b0d1621b3e8218b862ae7cbaea1
cadullms <cadullms@github.com>
2021-02-25 20:55:01 +0100

    reverting last commit, went into wrong PR...

--- terraform/team/azuresqldbpw.tf
+++ terraform/team/azuresqldbpw.tf
@@ -1,7 +1,5 @@
 resource "random_string" "azuresqldbpw" {
-  length = 16
-  special = false
-  min_upper = "2"
-  min_lower = "2"
-  min_numeric = "2"
+  length = 14
+  special = true
+  upper = true
 }
